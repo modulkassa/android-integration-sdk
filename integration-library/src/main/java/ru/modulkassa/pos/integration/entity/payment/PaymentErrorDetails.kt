42a173d8a3e953436e5f99bc03b3c373196af8e2
Yurii Anisimov <31529762+an-yurii@users.noreply.github.com>
2019-11-29 13:35:33 +0700

    MK-10877 Дополнить структуру ответа об ошибке платежной системы

--- integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/PaymentErrorDetails.kt
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/PaymentErrorDetails.kt
@@ -14,12 +14,27 @@ data class PaymentErrorDetails(
     /**
      * Текст сообщения для пользователя
      */
-    val message: String = ""
+    val message: String = "",
+    /**
+     * Код ответа платежного модуля
+     */
+    val responseCode: String = "---",
+    /**
+     * Код ответа хоста
+     */
+    val hostAnswerCode: String = "---",
+    /**
+     * Код ответа эмитента
+     */
+    val issuerAnswerCode: String = "---"
 ) : Bundable {
 
     companion object {
         private const val KEY_SLIP = "slip"
         private const val KEY_MESSAGE = "error_message"
+        private const val KEY_RESPONSE_CODE = "response_code"
+        private const val KEY_HOST_ANSWER_CODE = "host_answer_code"
+        private const val KEY_ISSUER_ANSWER_CODE = "issuer_answer_code"
 
         fun fromBundle(bundle: Bundle): PaymentErrorDetails {
             return PaymentErrorDetails(
@@ -33,6 +48,9 @@ data class PaymentErrorDetails(
         return Bundle().apply {
             putStringArrayList(KEY_SLIP, ArrayList(slip))
             putString(KEY_MESSAGE, message)
+            putString(KEY_RESPONSE_CODE, responseCode)
+            putString(KEY_HOST_ANSWER_CODE, hostAnswerCode)
+            putString(KEY_ISSUER_ANSWER_CODE, issuerAnswerCode)
         }
     }
 
--- integration-library/src/test/java/ru/modulkassa/pos/integration/entity/payment/PaymentErrorDetailsTest.kt
+++ integration-library/src/test/java/ru/modulkassa/pos/integration/entity/payment/PaymentErrorDetailsTest.kt
@@ -30,6 +30,9 @@ class PaymentErrorDetailsTest {
 
         assertThat(result.slip, equalTo(listOf("some text")))
         assertThat(result.message, equalTo("some message"))
+        assertThat(result.responseCode, equalTo("---"))
+        assertThat(result.hostAnswerCode, equalTo("---"))
+        assertThat(result.issuerAnswerCode, equalTo("---"))
     }
 
     @Test
@@ -40,6 +43,18 @@ class PaymentErrorDetailsTest {
 
         assertThat(bundle.getStringArrayList("slip"), equalTo(arrayListOf("some text")))
         assertThat(bundle.getString("error_message"), equalTo("message"))
+    }
+
+    @Test
+    fun ToBundle_ByDefault_SavesResponseCodes() {
+        val failedResult = PaymentErrorDetails(slip = emptyList(), responseCode = "response code",
+            hostAnswerCode = "host code", issuerAnswerCode = "issuer code")
+
+        val bundle = failedResult.toBundle()
+
+        assertThat(bundle.getString("response_code"), equalTo("response code"))
+        assertThat(bundle.getString("host_answer_code"), equalTo("host code"))
+        assertThat(bundle.getString("issuer_answer_code"), equalTo("issuer code"))
     }
 
 }

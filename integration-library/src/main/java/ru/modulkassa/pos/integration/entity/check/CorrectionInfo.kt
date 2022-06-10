e9beee2baeff18fc6cdec6fbd88098a18745e6bf
antashevia <I.Antashev@modulbank.ru>
2022-06-10 12:53:23 +0700

    MK-12894 Поддержка новых чеков коррекции в СДК

---
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/entity/check/CorrectionInfo.kt
@@ -0,0 +1,21 @@
+package ru.modulkassa.pos.integration.entity.check
+
+data class CorrectionInfo(
+    /**
+     * Причина коррекции
+     */
+    val reason: CorrectionReason,
+    /**
+     * Дата документа основания в формате ISO 8601
+     * Например: 2022-05-19T10:25:10
+     */
+    val documentDate: String,
+    /**
+     * Номер документа основания
+     */
+    val documentNum: String,
+    /**
+     * Фискальный признак документа основания
+     */
+    val fiscalSign: String?
+)
---
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/entity/check/CorrectionReason.kt
@@ -0,0 +1,13 @@
+package ru.modulkassa.pos.integration.entity.check
+
+enum class CorrectionReason {
+    /**
+     * Самостоятельная
+     */
+    INDEPENDENT,
+
+    /**
+     * По предписанию
+     */
+    BY_ORDER
+}

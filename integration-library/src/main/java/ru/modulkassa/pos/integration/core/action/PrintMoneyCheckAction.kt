edd73ec6b3b2be02a6fac16fb0dfbc5bd645e247
Ansar Kadyrov <a.kadyrov@modulbank.ru>
2023-02-27 16:53:25 +0500

    Поправить замечания

--- integration-library/src/main/java/ru/modulkassa/pos/integration/core/action/PrintMoneyCheckAction.kt
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/core/action/PrintMoneyCheckAction.kt
@@ -1,7 +1,6 @@
 package ru.modulkassa.pos.integration.core.action
 
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import ru.modulkassa.pos.integration.entity.kkt.MoneyCheck
 import ru.modulkassa.pos.integration.service.IModulKassa
 import ru.modulkassa.pos.integration.service.IModulKassaOperationCallback
@@ -13,21 +12,17 @@ class PrintMoneyCheckAction(
     private val gson = Gson()
 
     companion object {
-        // TODO: сделать это публичным и использовать в executor-е МодульКассы
         private const val NAME = "PRINT_MONEY_CHECK"
     }
 
-    override fun execute(kassa: IModulKassa, callback: ActionCallback<Boolean>?){
+    override fun execute(kassa: IModulKassa, callback: ActionCallback<Boolean>?) {
         kassa.executeOperation(NAME, gson.toJson(moneyCheck), object : IModulKassaOperationCallback.Stub() {
             override fun succeed(data: String?) {
                 callback?.succeed(true)
             }
 
             override fun failed(message: String, extraData: String?) {
-                @Suppress("EmptyClassBlock")
-                val type = object : TypeToken<Map<String, String>>() {
-                }.type
-                callback?.failed(message, gson.fromJson(extraData, type))
+                callback?.failed(message, null)
             }
         })
     }

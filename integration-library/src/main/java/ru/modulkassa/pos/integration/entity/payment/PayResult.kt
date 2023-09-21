1c7ac992fc67a94ecafb72259256ff8784118bd0
Anastasia Koreshkova <asya.koreshkova@gmail.com>
2023-09-21 23:42:36 +0700

    MK-13795 Запрос создания корзины покупки по эл серт. на сервер и обработка ответа

--- integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/PayResult.kt
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/PayResult.kt
@@ -3,6 +3,7 @@ package ru.modulkassa.pos.integration.entity.payment
 import android.os.Bundle
 import ru.modulkassa.pos.integration.entity.Bundable
 import ru.modulkassa.pos.integration.entity.payment.PaymentType.CARD
+import java.math.BigDecimal
 
 /**
  * Данные результата успешной оплаты
@@ -23,6 +24,10 @@ data class PayResult(
      */
     val paymentInfo: String? = null,
     /**
+     * Полная сумма по транзакции
+     */
+    val amount: BigDecimal? = null,
+    /**
      * Тип оплаты
      */
     val paymentType: PaymentType = CARD,
@@ -37,12 +42,14 @@ data class PayResult(
         private const val KEY_SLIP = "slip"
         private const val KEY_PAYMENT_INFO = "payment_info"
         private const val KEY_PAYMENT_TYPE = "payment_type"
+        private const val KEY_AMOUNT = "amount"
 
         fun fromBundle(bundle: Bundle): PayResult {
             return PayResult(
                 paymentCancelId = bundle.getString(KEY_CANCEL_ID) ?: "",
                 slip = bundle.getStringArrayList(KEY_SLIP) ?: arrayListOf(),
                 paymentInfo = bundle.getString(KEY_PAYMENT_INFO),
+                amount = BigDecimal(bundle.getString(KEY_AMOUNT, "0")),
                 paymentType = PaymentType.valueOf(bundle.getString(KEY_PAYMENT_TYPE) ?: "CARD"),
                 transactionDetails = TransactionDetails.fromBundle(bundle)
             )
@@ -54,6 +61,7 @@ data class PayResult(
             putString(KEY_CANCEL_ID, paymentCancelId)
             putStringArrayList(KEY_SLIP, ArrayList(slip))
             putString(KEY_PAYMENT_INFO, paymentInfo)
+            amount?.let { putString(KEY_AMOUNT, it.toPlainString()) }
             putString(KEY_PAYMENT_TYPE, paymentType.toString())
             putAll(transactionDetails?.toBundle() ?: Bundle.EMPTY)
             putString(RequestTypeSerialization.KEY, RequestType.PAY.name)

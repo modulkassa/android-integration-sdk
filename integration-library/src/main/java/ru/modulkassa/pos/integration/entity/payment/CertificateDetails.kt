0ce05f23078c761bd679c36b230041056865791c
Anastasia Koreshkova <42332031+AnastasiaKoreshkova@users.noreply.github.com>
2023-11-28 19:13:21 +0700

    MK-13979 Процесс возврата через скайтех с сертификатом (#50)

---
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/CertificateDetails.kt
@@ -0,0 +1,17 @@
+package ru.modulkassa.pos.integration.entity.payment
+
+import java.math.BigDecimal
+
+/**
+ * Информация для платежа/возврата с использованием электронного сертификата
+ */
+data class CertificateDetails(
+    /**
+     * Идентификатор сформированной корзины от НСПК
+     */
+    val basketId: String,
+    /**
+     * Сумма оплаты/возврата по электронному сертификату
+     */
+    val certificateAmount: BigDecimal
+)
--- integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/RefundResult.kt
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/RefundResult.kt
@@ -1,7 +1,10 @@
 package ru.modulkassa.pos.integration.entity.payment
 
 import android.os.Bundle
+import com.google.gson.reflect.TypeToken
 import ru.modulkassa.pos.integration.entity.Bundable
+import ru.modulkassa.pos.integration.entity.GsonFactory
+import java.math.BigDecimal
 
 /**
  * Данные результата успешного возврата
@@ -15,16 +18,31 @@ data class RefundResult(
     /**
      * Данные транзакции
      */
-    val transactionDetails: TransactionDetails? = null
+    val transactionDetails: TransactionDetails? = null,
+    /**
+     * Полная сумма по транзакции
+     */
+    val amount: BigDecimal? = null,
+    /**
+     * Данные для платежа с использованием электронного сертификата
+     */
+    val certificate: CertificateDetails? = null
 ) : Bundable {
 
     companion object {
         private const val KEY_SLIP = "slip"
+        private const val KEY_AMOUNT = "amount"
+        private const val KEY_CERT = "certificate"
+        private val gson = GsonFactory.provide()
 
         fun fromBundle(bundle: Bundle): RefundResult {
             return RefundResult(
                 slip = bundle.getStringArrayList(KEY_SLIP) ?: arrayListOf(),
-                transactionDetails = TransactionDetails.fromBundle(bundle)
+                transactionDetails = TransactionDetails.fromBundle(bundle),
+                amount = bundle.getString(KEY_AMOUNT)?.let { BigDecimal(it) },
+                certificate = bundle.getString(KEY_CERT)?.let {
+                    gson.fromJson(it, object : TypeToken<CertificateDetails>() {}.type)
+                }
             )
         }
     }
@@ -34,6 +52,8 @@ data class RefundResult(
             putStringArrayList(KEY_SLIP, ArrayList(slip))
             putAll(transactionDetails?.toBundle() ?: Bundle.EMPTY)
             putString(RequestTypeSerialization.KEY, RequestType.REFUND.name)
+            amount?.let { putString(KEY_AMOUNT, it.toPlainString()) }
+            certificate?.let { putString(KEY_CERT, gson.toJson(certificate)) }
         }
     }
 

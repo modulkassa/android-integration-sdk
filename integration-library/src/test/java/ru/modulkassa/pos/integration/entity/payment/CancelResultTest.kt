197d9a969548c6ee58b79d034a2f1dfd9b702cb9
Anastasia Koreshkova <asya.koreshkova@gmail.com>
2024-01-10 19:18:04 +0700

    MK-13912 Поддержать операцию отмену в теории. Возвращать из эквайринга ошибку на практике.

--- integration-library/src/test/java/ru/modulkassa/pos/integration/entity/payment/CancelResultTest.kt
+++ integration-library/src/test/java/ru/modulkassa/pos/integration/entity/payment/CancelResultTest.kt
@@ -3,10 +3,11 @@ package ru.modulkassa.pos.integration.entity.payment
 import android.os.Bundle
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.nullValue
-import org.junit.Assert.assertThat
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import java.math.BigDecimal
 
 @RunWith(RobolectricTestRunner::class)
 class CancelResultTest {
@@ -19,6 +20,8 @@ class CancelResultTest {
 
         assertThat(result.slip, equalTo(listOf()))
         assertThat(result.transactionDetails, nullValue())
+        assertThat(result.amount, nullValue())
+        assertThat(result.certificate, nullValue())
     }
 
     @Test
@@ -34,6 +37,19 @@ class CancelResultTest {
     }
 
     @Test
+    fun FromBundle_AmountAndCertificate_ReadsDataFromBundle() {
+        val bundle = Bundle().apply {
+            putString("amount", "12")
+            putString("certificate", "{\"basketId\":\"123456\",\"certificateAmount\":1}")
+        }
+
+        val result = CancelResult.fromBundle(bundle)
+
+        assertThat(result.amount, equalTo(BigDecimal.valueOf(12)))
+        assertThat(result.certificate, equalTo(CertificateDetails("123456", BigDecimal.ONE)))
+    }
+
+    @Test
     fun ToBundle_ByDefault_SavesSlip() {
         val result = CancelResult(listOf("some text"))
 
@@ -41,6 +57,8 @@ class CancelResultTest {
 
         assertThat(bundle.getStringArrayList("slip")?.toList(), equalTo(listOf("some text")))
         assertThat(bundle.keySet().any { it.startsWith("transaction_details") }, equalTo(false))
+        assertThat(bundle.getString("payment_info"), nullValue())
+        assertThat(bundle.getString("certificate"), nullValue())
     }
 
     @Test
@@ -63,6 +81,20 @@ class CancelResultTest {
 
         assertThat(bundle.getStringArrayList("slip")?.toList(), equalTo(listOf("some text")))
         assertThat(bundle.keySet().any { it.startsWith("transaction_details") }, equalTo(true))
+    }
+
+    @Test
+    fun ToBundle_AmountAndCertificate_SavesFields() {
+        val result = CancelResult(
+            slip = emptyList(),
+            amount = BigDecimal.TEN,
+            certificate = CertificateDetails("123456", BigDecimal.ONE)
+        )
+
+        val bundle = result.toBundle()
+
+        assertThat(bundle.getString("amount"), equalTo("10"))
+        assertThat(bundle.getString("certificate"), equalTo("{\"basketId\":\"123456\",\"certificateAmount\":1}"))
     }
 
 }

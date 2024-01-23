619562b2e8be9e78b5c8bc79cae8362f07902cae
Anastasia Koreshkova <42332031+AnastasiaKoreshkova@users.noreply.github.com>
2024-01-23 16:18:01 +0700

    MK-13997 [Android] Платежные сертификаты МИР: Заглушка на отмену платежа (оплаты/возврата) в случае ошибки фискализации

--- integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/CancelResult.kt
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/CancelResult.kt
@@ -1,7 +1,11 @@
 package ru.modulkassa.pos.integration.entity.payment
 
 import android.os.Bundle
+import com.google.gson.reflect.TypeToken
 import ru.modulkassa.pos.integration.entity.Bundable
+import ru.modulkassa.pos.integration.entity.GsonFactory
+import ru.modulkassa.pos.integration.entity.payment.RefundResult.Companion
+import java.math.BigDecimal
 
 /**
  * Данные результата успешной отмены
@@ -15,16 +19,32 @@ data class CancelResult(
     /**
      * Данные транзакции
      */
-    val transactionDetails: TransactionDetails? = null
+    val transactionDetails: TransactionDetails? = null,
+    /**
+     * Полная сумма по транзакции
+     */
+    val amount: BigDecimal? = null,
+    /**
+     * Данные для платежа с использованием электронного сертификата
+     */
+    val certificate: CertificateDetails? = null
 ) : Bundable {
 
     companion object {
         private const val KEY_SLIP = "slip"
+        private const val KEY_AMOUNT = "amount"
+        private const val KEY_CERT = "certificate"
+
+        private val gson = GsonFactory.provide()
 
         fun fromBundle(bundle: Bundle): CancelResult {
             return CancelResult(
                 slip = bundle.getStringArrayList(KEY_SLIP) ?: arrayListOf(),
-                transactionDetails = TransactionDetails.fromBundle(bundle)
+                transactionDetails = TransactionDetails.fromBundle(bundle),
+                amount = bundle.getString(KEY_AMOUNT)?.let { BigDecimal(it) },
+                certificate = bundle.getString(KEY_CERT)?.let {
+                    gson.fromJson(it, object : TypeToken<CertificateDetails>() {}.type)
+                }
             )
         }
     }
@@ -34,6 +54,8 @@ data class CancelResult(
             putStringArrayList(KEY_SLIP, ArrayList(slip))
             putAll(transactionDetails?.toBundle() ?: Bundle.EMPTY)
             putString(RequestTypeSerialization.KEY, RequestType.CANCEL.name)
+            amount?.let { putString(KEY_AMOUNT, it.toPlainString()) }
+            certificate?.let { putString(KEY_CERT, gson.toJson(certificate)) }
         }
     }
 }

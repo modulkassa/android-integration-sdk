4ff33cff032973953596a30c8c8fcf88164d4a1f
Anastasia Koreshkova <a.koreshkova@modulbank.ru>
2025-06-26 12:41:34 +0700

    v 1.7.15_MK-15042_1

---
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/entity/crm/CrmPosition.kt
@@ -0,0 +1,27 @@
+package ru.modulkassa.pos.integration.entity.crm
+
+import java.math.BigDecimal
+
+data class CrmPosition(
+    /**
+     * Штрихкод товара
+     */
+    var barcode: String,
+    /**
+     * Количество товара
+     */
+    var quantity: BigDecimal,
+    /**
+     * Цена товара
+     */
+    var price: BigDecimal,
+    /**
+     * Список модификаторов
+     */
+    val modifiers: List<CrmModifier>,
+    /**
+     * Данные поставщика
+     * Заполняются crm системой
+     */
+    val supplierInfo: SupplierInfo? = null,
+)
--- integration-library/src/test/java/ru/modulkassa/pos/integration/entity/crm/CreateOrderToCrmResultTest.kt
+++ integration-library/src/test/java/ru/modulkassa/pos/integration/entity/crm/CreateOrderToCrmResultTest.kt
@@ -3,10 +3,11 @@ package ru.modulkassa.pos.integration.entity.crm
 import android.os.Bundle
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.nullValue
-import org.junit.Assert.assertThat
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import java.math.BigDecimal
 
 @RunWith(RobolectricTestRunner::class)
 class CreateOrderToCrmResultTest {
@@ -42,6 +43,35 @@ class CreateOrderToCrmResultTest {
     }
 
     @Test
+    fun ToBundle_WithPositions_SavesToBundle() {
+        val createOrderToCrmResult = CreateOrderToCrmResult(
+            orderId = 12345,
+            phone = "+70008887766",
+            positions = listOf(CrmPosition("barcode", BigDecimal.ONE, BigDecimal.ONE, listOf()))
+        )
+
+        val bundle = createOrderToCrmResult.toBundle()
+
+        assertThat(
+            bundle.getString("positions"),
+            equalTo("[{\"barcode\":\"barcode\",\"quantity\":1,\"price\":1,\"modifiers\":[]}]")
+        )
+    }
+
+    @Test
+    fun ToBundle_WithoutPositions_SavesToBundle() {
+        val createOrderToCrmResult = CreateOrderToCrmResult(
+            orderId = 12345,
+            positions = null,
+            phone = "+70008887766"
+        )
+
+        val bundle = createOrderToCrmResult.toBundle()
+
+        assertThat(bundle.getString("positions"), equalTo("null"))
+    }
+
+    @Test
     fun FromBundle_WithPromouter_CreatesResult() {
         val bundle = Bundle().apply {
             putInt("orderId", 12345)
@@ -71,6 +101,36 @@ class CreateOrderToCrmResultTest {
     }
 
     @Test
+    fun FromBundle_WithPositions_CreatesResult() {
+        val bundle = Bundle().apply {
+            putInt("orderId", 12345)
+            putString("phone", "+70008887766")
+            putString("positions", "[{\"barcode\":\"barcode\",\"quantity\":1,\"price\":1,\"modifiers\":[]}]")
+        }
+
+        val createOrderToCrmResult = CreateOrderToCrmResult.fromBundle(bundle)
+
+        assertThat(
+            createOrderToCrmResult.positions,
+            equalTo(listOf(CrmPosition("barcode", BigDecimal.ONE, BigDecimal.ONE, listOf())))
+        )
+    }
+
+    @Test
+    fun FromBundle_WithoutPositions_CreatesResult() {
+        val bundle = Bundle().apply {
+            putInt("orderId", 12345)
+            putString("phone", "+70008887766")
+            putString("positions", "null")
+        }
+
+        val createOrderToCrmResult = CreateOrderToCrmResult.fromBundle(bundle)
+
+        assertThat(createOrderToCrmResult.positions, nullValue())
+    }
+
+
+    @Test
     fun FromBundleToBundle_WithoutPromouterId_CreatesResult() {
         val origin = CreateOrderToCrmResult(
             orderId = 12345,
@@ -85,6 +145,25 @@ class CreateOrderToCrmResultTest {
         assertThat(createOrderToCrmResult.orderId, equalTo(12345))
         assertThat(createOrderToCrmResult.promouterId, nullValue())
         assertThat(createOrderToCrmResult.phone, equalTo("+70008887766"))
+    }
+
+    @Test
+    fun FromBundleToBundle_WithoutPositions_CreatesResult() {
+        val origin = CreateOrderToCrmResult(
+            orderId = 12345,
+            promouterId = null,
+            phone = "+70008887766",
+            positions = null
+        )
+
+        val bundle = origin.toBundle()
+
+        val createOrderToCrmResult = CreateOrderToCrmResult.fromBundle(bundle)
+
+        assertThat(createOrderToCrmResult.orderId, equalTo(12345))
+        assertThat(createOrderToCrmResult.promouterId, nullValue())
+        assertThat(createOrderToCrmResult.phone, equalTo("+70008887766"))
+        assertThat(createOrderToCrmResult.positions, nullValue())
     }
 
 }

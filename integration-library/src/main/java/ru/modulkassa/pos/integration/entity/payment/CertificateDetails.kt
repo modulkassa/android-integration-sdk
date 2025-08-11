26a6848e43b2c03c67203d3712f814aff8d965f6
Anastasia Koreshkova <42332031+AnastasiaKoreshkova@users.noreply.github.com>
2025-08-11 15:13:53 +0700

    MK-15090 [Android, ФЭС] "Неизвестная ошибка. Обратитесь в поддержку" при оплате электронными сертификатами через SSP v2 (#74)
    Co-authored-by: Anastasia Koreshkova <a.koreshkova@modulbank.ru>

--- integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/CertificateDetails.kt
+++ integration-library/src/main/java/ru/modulkassa/pos/integration/entity/payment/CertificateDetails.kt
@@ -1,5 +1,6 @@
 package ru.modulkassa.pos.integration.entity.payment
 
+import com.google.gson.annotations.SerializedName
 import java.math.BigDecimal
 
 /**
@@ -10,9 +11,11 @@ data class CertificateDetails(
      * Идентификатор сформированной корзины от НСПК
      * Формат: 24 цифры
      */
+    @SerializedName("basketId")
     val basketId: String,
     /**
      * Сумма оплаты/возврата по электронному сертификату
      */
+    @SerializedName("certificateAmount")
     val certificateAmount: BigDecimal
 )

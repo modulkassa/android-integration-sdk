4ed67a76512e0bc133e5e2a647589b4fdb9e1cfc
Anastasia Koreshkova <a.koreshkova@modulbank.ru>
2025-10-09 21:41:22 +0700

    Revert "MK-14860 [Android] Разрешительный режим. II этап - Офлайн проверка марки (#72)"
    This reverts commit 90e1c2898527017c8e949393582fa85b278ab297.
    
    # Conflicts:
    #	integration-library/build.gradle

--- integration-library/src/main/java/ru/modulkassa/pos/integration/entity/mark/MarkRequest.kt
+++
@@ -1,8 +0,0 @@
-package ru.modulkassa.pos.integration.entity.mark
-
-interface MarkRequest {
-    /**
-     * Тип запроса к офлайн системе обработки марок
-     */
-    val requestType: MarkRequestType
-}
